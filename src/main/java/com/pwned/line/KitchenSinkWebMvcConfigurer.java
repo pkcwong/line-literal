620e453e02429405ad190d5a1ca0db87e3a633b6
Christopher Wong <christopherpk1996@gmail.com>
2017-10-03 09:48:42 +0800

    gradle stage

--- src/main/java/com/pwned/line/KitchenSinkWebMvcConfigurer.java
+++ src/main/java/com/pwned/line/KitchenSinkWebMvcConfigurer.java
@@ -1,18 +1,15 @@
 package com.pwned.line;
 
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
-@Slf4j
 public class KitchenSinkWebMvcConfigurer extends WebMvcConfigurerAdapter {
 
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
 		String downloadedContentUri = KitchenSinkApplication.downloadedContentDir.toUri().toASCIIString();
-		//log.info("downloaded dir: {}", downloadedContentUri);
 		registry.addResourceHandler("/downloaded/**").addResourceLocations(downloadedContentUri);
 		registry.addResourceHandler("/static/**").addResourceLocations("classpath:/static/");
 	}
