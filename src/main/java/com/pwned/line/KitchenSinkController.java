f7a258cc634a3a2a8fddedace825eba5c6756099
Christopher Wong <christopherpk1996@gmail.com>
2017-10-09 00:46:35 +0800

    api-ai struct

--- src/main/java/com/pwned/line/KitchenSinkController.java
+++ src/main/java/com/pwned/line/KitchenSinkController.java
@@ -5,7 +5,6 @@ import com.linecorp.bot.model.ReplyMessage;
 import com.linecorp.bot.model.event.MessageEvent;
 import com.linecorp.bot.model.event.message.TextMessageContent;
 import com.linecorp.bot.model.message.Message;
-import com.linecorp.bot.model.message.TextMessage;
 import com.linecorp.bot.spring.boot.annotation.EventMapping;
 import com.linecorp.bot.spring.boot.annotation.LineMessageHandler;
 import com.pwned.line.handler.TextHandler;
---
+++ src/main/java/com/pwned/line/interpreter/ApiAI.java
@@ -0,0 +1,9 @@
+package com.pwned.line.interpreter;
+
+public class ApiAI {
+
+	private static final String BASE_URL = "https://api.api.ai/v1/";
+	private static final String DEVELOPER_ACCESS_TOKEN = "c46e4802657548dcb52bd8cab3d42a56";
+	private static final String VERSION = "20170712";
+
+}
