b537037a9e530ef8a8a26df9a6fb4dd31c85ab0a
Christopher Wong <christopherpk1996@gmail.com>
2017-10-10 11:21:23 +0800

    RequestMethod.GET controller

--- src/main/java/com/pwned/line/http/RequestController.java
+++ src/main/java/com/pwned/line/http/RequestController.java
@@ -1,17 +1,19 @@
 package com.pwned.line.http;
 
 import org.json.JSONObject;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
 
+@Controller
 public class RequestController {
 
 	@RequestMapping(value = {"/"}, method = {RequestMethod.GET})
 	@ResponseBody
-	public String postRequest(HttpServletRequest request) {
+	public String getRequest(HttpServletRequest request) {
 		JSONObject json = new JSONObject(request.getParameterMap());
 		return json.toString();
 	}
