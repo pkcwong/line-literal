edcef0f753ff84148f28938b487accf27efbc517
Christopher Wong <christopherpk1996@gmail.com>
2017-10-10 15:31:55 +0800

    rebuild

--- src/main/java/com/pwned/line/KitchenSinkController.java
+++ src/main/java/com/pwned/line/KitchenSinkController.java
@@ -1,6 +1,6 @@
 package com.pwned.line;
 
-import com.linecorp.bot.client.LineMessagingClient;
+import com.linecorp.bot.client.LineMessagingServiceBuilder;
 import com.linecorp.bot.model.ReplyMessage;
 import com.linecorp.bot.model.event.MessageEvent;
 import com.linecorp.bot.model.event.message.TextMessageContent;
@@ -8,32 +8,23 @@ import com.linecorp.bot.model.message.Message;
 import com.linecorp.bot.spring.boot.annotation.EventMapping;
 import com.linecorp.bot.spring.boot.annotation.LineMessageHandler;
 import com.pwned.line.handler.TextHandler;
-import org.springframework.beans.factory.annotation.Autowired;
 
-import java.util.Collections;
-import java.util.List;
-import java.util.concurrent.ExecutionException;
+import java.io.IOException;
 
 @LineMessageHandler
 public class KitchenSinkController {
 
-	@Autowired
-	private static LineMessagingClient lineMessagingClient;
-
 	@EventMapping
-	public void handleTextMessageEvent(MessageEvent<TextMessageContent> event) throws Exception {
+	public static void handleTextMessageEvent(MessageEvent<TextMessageContent> event) throws Exception {
 		TextHandler.handle(event);
 	}
 
 	public static void reply(String replyToken, Message message) {
-		KitchenSinkController.reply(replyToken, Collections.singletonList(message));
-	}
-
-	public static void reply(String replyToken, List<Message> messages) {
 		try {
-			lineMessagingClient.replyMessage(new ReplyMessage(replyToken, messages)).get();
-		} catch (InterruptedException | ExecutionException e) {
-			throw new RuntimeException(e);
+			ReplyMessage replyMessage = new ReplyMessage(replyToken, message);
+			LineMessagingServiceBuilder.create(System.getenv("LINE_BOT_CHANNEL_TOKEN")).build().replyMessage(replyMessage).execute();
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
 	}
 
