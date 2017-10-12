1388404083928b8e9abb4c5ae2179e23ed59cddf
Christopher Wong <christopherpk1996@gmail.com>
2017-10-12 09:41:09 +0800

    debug

--- src/main/java/com/pwned/line/interpreter/ApiAI.java
+++ src/main/java/com/pwned/line/interpreter/ApiAI.java
@@ -25,6 +25,7 @@ public class ApiAI {
 			http.setParams("query", query);
 			http.setParams("sessionId", replyToken);
 			JSONObject json = new JSONObject(http.get());
+			System.out.println(json);
 			ApiAI.handler(json);
 		} catch (Exception e) {
 			e.printStackTrace();
