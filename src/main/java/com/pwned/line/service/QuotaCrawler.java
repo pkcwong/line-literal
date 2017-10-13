4d53a4e2e3c73d8301234a85dd190b52c6c43993
cjwku1209 <cjwku@connect.ust.hk>
2017-10-13 17:30:47 +0800

    Final and Docs

--- src/main/java/com/pwned/line/service/QuotaCrawler.java
+++ src/main/java/com/pwned/line/service/QuotaCrawler.java
@@ -8,32 +8,45 @@ import java.util.regex.Pattern;
 /***
  * Service for sending requests to HKUST QUOTA WEBSITE.
  *  * Required params: [DEPARTMENT, COURSE_CODE]
- * @author Christopher Wong, Calvin Ku
+ * @author Calvin Ku, Christopher Wong
  */
 public class QuotaCrawler extends Service{
 	private static final String QUOTA_URL = "https://w5.ab.ust.hk/wcq/cgi-bin/1710/subject/";
 
-	public QuotaCrawler(String query){ super (query); }
-// The course pre-requisite for @courseid is
+	public QuotaCrawler(String query){
+		super (query);
+	}
+
+	/***
+	 * Fetch the course information from HKUST's Quota Page.
+	 * @return
+	 */
 	@Override
 	public String resolve() {
 		String department = super.getArgs("DEPARTMENT").toString();
 		String courseCode = super.getArgs("COURSE_CODE").toString();
+		HTTP httpClient = new HTTP(QUOTA_URL + department);
+		super.fulfillment = getCourseName(httpClient.get());
+		return super.fulfillment;
+	}
+
+	/***
+	 * Returns the name of the course.
+	 * @param httpResponse http response
+	 * @return Course name
+	 */
+	private String getCourseName(String httpResponse) {
+		String department = super.getArgs("DEPARTMENT").toString();
+		String courseCode = super.getArgs("COURSE_CODE").toString();
 		String regex = "<h2>" + department + "\\s" + courseCode + "\\s-\\s(.+?)\\s\\(\\d\\sunits\\)</h2>";
 		String courseName = "";
-		HTTP httpClient = new HTTP(QUOTA_URL + department);
 		Pattern departmentPattern = Pattern.compile(regex);
-		Matcher courseMatcher = departmentPattern.matcher(httpClient.get());
+		Matcher courseMatcher = departmentPattern.matcher(httpResponse);
 		while(courseMatcher.find()){
 			System.out.println(regex);
 			courseName = courseMatcher.group(1);
 		}
-		super.fulfillment = super.fulfillment.replace("@data", courseName);
-		return super.fulfillment;
-	}
-
-	private String getCourseName(String httpResponse) {
-		return null;
+		return super.fulfillment.replace("@data", courseName);
 	}
 
 }
