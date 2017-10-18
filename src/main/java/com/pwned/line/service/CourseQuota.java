086675b154d690b5c58f86943292ecccb3a3490f
Christopher Wong <christopherpk1996@gmail.com>
2017-10-18 11:50:57 +0800

    docs and final

--- src/main/java/com/pwned/line/service/CourseQuota.java
+++ src/main/java/com/pwned/line/service/CourseQuota.java
@@ -8,6 +8,11 @@ import java.util.concurrent.CompletableFuture;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+/***
+ * Service for sending requests to HKUST QUOTA WEBSITE.
+ * Required params: [DEPARTMENT, COURSE_CODE]
+ * @author Calvin Ku, Christopher Wong
+ */
 public class CourseQuota implements Service {
 
 	private static final String QUOTA_URL = "https://w5.ab.ust.hk/wcq/cgi-bin/1710/subject/";
@@ -30,6 +35,10 @@ public class CourseQuota implements Service {
 		this.args = service.getArgs();
 	}
 
+	/***
+	 * Fetch the course information from HKUST Course Quota Page.
+	 * @return instance
+	 */
 	@Override
 	public CompletableFuture<Service> resolve() {
 		String department = this.getParam("DEPARTMENT").toString();
@@ -38,6 +47,11 @@ public class CourseQuota implements Service {
 		return CompletableFuture.supplyAsync(() -> this);
 	}
 
+	/***
+	 * Returns the name of the course.
+	 * @param httpResponse http response
+	 * @return Course name
+	 */
 	private String getCourseName(String httpResponse) {
 		String department = this.getParam("DEPARTMENT").toString();
 		String courseCode = this.getParam("COURSE_CODE").toString();
