289d82e415f0fea2d4cbe1daf45effb7e21d2bfa
Christopher Wong <christopherpk1996@gmail.com>
2017-10-24 16:08:30 +0800

    yandex docs

--- src/main/java/com/pwned/line/service/Translate.java
+++ src/main/java/com/pwned/line/service/Translate.java
@@ -4,6 +4,13 @@ import com.pwned.line.web.ApiAI;
 import com.pwned.line.web.YandexTranslate;
 import org.json.JSONObject;
 
+/***
+ * Service module for translation.
+ * Required params: [uid]
+ * Reserved tokens: [@translate]
+ * Resolved params: []
+ * @author Christopher Wong, Calvin Ku
+ */
 public class Translate extends DefaultService {
 
 	private static String API_AI_ACCESS_TOKEN = "6ad9c104380b49d6803939577310156a";
@@ -17,8 +24,8 @@ public class Translate extends DefaultService {
 	public void payload() throws Exception {
 		JSONObject apiai = new ApiAI(API_AI_ACCESS_TOKEN, this.getParam("uid").toString(), this.fulfillment).execute();
 		JSONObject parameters = apiai.getJSONObject("result").getJSONObject("parameters");
-		JSONObject yandex = new YandexTranslate(YANDEX_ACCESS_TOKEN, this.fulfillment, parameters.getString("lang-to")).execute();
-		this.fulfillment = yandex.getJSONArray("text").get(0).toString();
+		JSONObject yandex = new YandexTranslate(YANDEX_ACCESS_TOKEN, parameters.getString("phrase"), parameters.getString("lang-to")).execute();
+		this.fulfillment = this.fulfillment.replace("@translate", yandex.getJSONArray("text").get(0).toString());
 	}
 
 	@Override
