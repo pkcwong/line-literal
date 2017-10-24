a1420ece3a72754b0468c3f12da05e1ff9cc1b70
Christopher Wong <christopherpk1996@gmail.com>
2017-10-24 15:00:24 +0800

    cleanup

--- src/main/java/com/pwned/line/service/DefaultService.java
+++ src/main/java/com/pwned/line/service/DefaultService.java
@@ -63,7 +63,7 @@ public class DefaultService implements Service {
 	 */
 	@Override
 	public Service chain() throws Exception {
-		return new DialogFlowCourse(this).resolve().get();
+		return new CourseName(this).resolve().get();
 	}
 
 	@Override
