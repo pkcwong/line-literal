b521f02083fa8aa997ddef297e8dbc56bae9fd8b
Christopher Wong <christopherpk1996@gmail.com>
2017-10-24 17:03:51 +0800

    split

---
+++ src/main/java/com/pwned/line/service/DialogFlowTranslate.java
@@ -0,0 +1,26 @@
+package com.pwned.line.service;
+
+import com.pwned.line.web.ApiAI;
+import org.json.JSONObject;
+
+public class DialogFlowTranslate extends DefaultService {
+
+	private static String API_AI_ACCESS_TOKEN = "6ad9c104380b49d6803939577310156a";
+
+	public DialogFlowTranslate(Service service) {
+		super(service);
+	}
+
+	@Override
+	public void payload() throws Exception {
+		JSONObject response = new ApiAI(API_AI_ACCESS_TOKEN, this.getParam("uid").toString(), this.fulfillment).execute();
+		this.setParam("parameters", response.getJSONObject("result").getJSONObject("parameters"));
+		this.fulfillment = response.getJSONObject("result").getJSONObject("fulfillment").getString("speech");
+	}
+
+	@Override
+	public Service chain() throws Exception {
+		return new Translate(this).resolve().get();
+	}
+
+}
--- src/main/java/com/pwned/line/service/Translate.java
+++ src/main/java/com/pwned/line/service/Translate.java
@@ -13,7 +13,6 @@ import org.json.JSONObject;
  */
 public class Translate extends DefaultService {
 
-	private static String API_AI_ACCESS_TOKEN = "6ad9c104380b49d6803939577310156a";
 	private static String YANDEX_ACCESS_TOKEN = "trnsl.1.1.20171018T105910Z.b2c2d71c4cea2b88.deb0f12c301616942345158b7773860e9b9b80d3";
 
 	public Translate(Service service) {
@@ -22,8 +21,7 @@ public class Translate extends DefaultService {
 
 	@Override
 	public void payload() throws Exception {
-		JSONObject apiai = new ApiAI(API_AI_ACCESS_TOKEN, this.getParam("uid").toString(), this.fulfillment).execute();
-		JSONObject parameters = apiai.getJSONObject("result").getJSONObject("parameters");
+		JSONObject parameters = new JSONObject(this.getParam("parameters").toString());
 		JSONObject yandex = new YandexTranslate(YANDEX_ACCESS_TOKEN, parameters.getString("phrase"), parameters.getString("lang-to")).execute();
 		this.fulfillment = yandex.getJSONArray("text").get(0).toString();
 	}
