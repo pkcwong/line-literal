8bc055f00b2967a19a136523e2e8ab32edb4dcbb
httpak <httpak@connect.ust.hk>
2017-10-28 18:18:12 +0800

    temperature is default at 27

--- src/main/java/com/pwned/line/service/Weather.java
+++ src/main/java/com/pwned/line/service/Weather.java
@@ -41,9 +41,15 @@ public class Weather extends DefaultService{
 		//NodeList nList = weather_xml_document.getElementsByTagName("tr");
 		weather_string.indexOf("Bulletin");
 		String time = weather_string.substring(weather_string.indexOf("<pubDate>") + 9, weather_string.indexOf("</pubDate>") - 1);
-		String temperature = weather_string.substring(weather_string.indexOf("Air temperature : ") + 18, weather_string.indexOf("degrees Celsius<br/>") - 1);
+		String temperature = "";
+		if(weather_string.indexOf("degrees Celsius<br/>") - 1 > weather_string.indexOf("Air temperature : ") + 18){
+			temperature = weather_string.substring(weather_string.indexOf("Air temperature : ") + 18, weather_string.indexOf("degrees Celsius<br/>") - 1);
+		}
+		if(temperature == ""){
+			temperature = "27";
+		}
 		String weather_reply = "The temperate of " + city + "is " + temperature + " degrees Celsius at " + time;
-		this.fulfillment = weather_reply;
+		this.fulfillment = temperature;
 	}
 
 	/*public static Document loadXMLFromString(String xml) throws Exception{
