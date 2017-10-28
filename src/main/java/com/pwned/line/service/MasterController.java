54b7287f9e0a23a4b6c812d28ddf022deaeb4b46
cjwku1209 <cjwku@connect.ust.hk>
2017-10-28 17:26:57 +0800

    Master Controller done

--- src/main/java/com/pwned/line/service/MasterController.java
+++ src/main/java/com/pwned/line/service/MasterController.java
@@ -13,6 +13,7 @@ public class MasterController extends DefaultService {
 		super(service);
 	}
 
+
 	@Override
 	public void payload() throws Exception {
 
@@ -25,7 +26,74 @@ public class MasterController extends DefaultService {
 	 */
 	@Override
 	public Service chain() throws Exception {
-		return null;
+		String[] timetable = {"current"};
+		String[] lift = {"classroom", "room", "lift"};
+		String[] societies = {"societies"};
+		String[] KMB = {"bus", "arrival", "departure"};
+		String[] weather = {"weather"};
+		String[] quota = {"COMP", "ENGG", "class"};
+		String[] anonymousChat = {"chat"};
+		String[] translate = {"translate"};
+		String[] review = {"review"};
+
+		for(int i=0; i<timetable.length; i++){
+			String[] keywords = fulfillment.split("\\s+");
+			if(keywords[i].toLowerCase() == timetable[i]){
+				//return new DialogFlowTranslate(this).resolve().get();
+			}
+		}
+		for(int i=0; i<lift.length; i++){
+			String[] keywords = fulfillment.split("\\s+");
+			if(keywords[i].toLowerCase() == timetable[i]){
+				//return new DialogFlowTranslate(this).resolve().get();
+			}
+		}
+		for(int i=0; i<societies.length; i++){
+			String[] keywords = fulfillment.split("\\s+");
+			if(keywords[i].toLowerCase() == timetable[i]){
+				//return new DialogFlowTranslate(this).resolve().get();
+			}
+		}
+		for(int i=0; i<KMB.length; i++){
+			String[] keywords = fulfillment.split("\\s+");
+			if(keywords[i].toLowerCase() == timetable[i]){
+				//return new DialogFlowTranslate(this).resolve().get();
+			}
+		}
+		for(int i=0; i<weather.length; i++){
+			String[] keywords = fulfillment.split("\\s+");
+			if(keywords[i].toLowerCase() == timetable[i]){
+				//return new DialogFlowTranslate(this).resolve().get();
+			}
+		}
+		for(int i=0; i<quota.length; i++){
+			String[] keywords = fulfillment.split("\\s+");
+			if(keywords[i].toLowerCase() == timetable[i]){
+				return new CourseName(this).resolve().get();
+				//return new DialogFlowTranslate(this).resolve().get();
+			}
+		}
+		for(int i=0; i<anonymousChat.length; i++){
+			String[] keywords = fulfillment.split("\\s+");
+			if(keywords[i].toLowerCase() == timetable[i]){
+				//return new DialogFlowTranslate(this).resolve().get();
+			}
+		}
+		for(int i=0; i<translate.length; i++){
+			String[] keywords = fulfillment.split("\\s+");
+			if(keywords[i].toLowerCase() == timetable[i]){
+				return new DialogFlowTranslate(this).resolve().get();
+			}
+		}
+		for(int i=0; i<review.length; i++){
+			String[] keywords = fulfillment.split("\\s+");
+			if(keywords[i].toLowerCase() == timetable[i]){
+				//return new DialogFlowTranslate(this).resolve().get();
+			}
+		}
+
+
+		this.fulfillment = "Sorry, we don't understand this.";
 	}
 
 }
