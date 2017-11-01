8053563c01267435a361d6020f93f7ee1ea6c16c
Christopher Wong <christopherpk1996@gmail.com>
2017-11-01 13:58:02 +0800

    confirm dialog

--- src/main/java/com/pwned/line/service/AnonymousChat.java
+++ src/main/java/com/pwned/line/service/AnonymousChat.java
@@ -1,5 +1,10 @@
 package com.pwned.line.service;
 
+import com.linecorp.bot.model.action.MessageAction;
+import com.linecorp.bot.model.message.TemplateMessage;
+import com.linecorp.bot.model.message.template.ConfirmTemplate;
+import com.pwned.line.KitchenSinkController;
+
 public class AnonymousChat extends DefaultService {
 
 	public AnonymousChat(Service service) {
@@ -8,7 +13,15 @@ public class AnonymousChat extends DefaultService {
 
 	@Override
 	public void payload() throws Exception {
-
+		ConfirmTemplate confirmTemplate = null;
+		if (this.getParam("uid").toString().equals(this.getParam("bind").toString())) {
+			this.fulfillment = "*** Tap \'connect\' to chat with a random user! ***";
+			confirmTemplate = new ConfirmTemplate("Anonymous Chat System", new MessageAction("connect", "Connect"), new MessageAction("cancel", "Cancel"));
+		} else {
+			this.fulfillment = "*** Tap \'Terminate\' to close the session. ***";;
+			confirmTemplate = new ConfirmTemplate("Anonymous Chat System", new MessageAction("terminate", "Terminate"), new MessageAction("cancel", "Cancel"));
+		}
+		KitchenSinkController.push(this.getParam("uid").toString(), new TemplateMessage("Anonymous Chat System", confirmTemplate));
 	}
 
 	@Override
