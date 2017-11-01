4b3cb6b133ddfb865a91bb79b9f59b9bf13e9f3f
Bear <Bear@BeardeMacBook-Pro.local>
2017-11-02 06:45:40 +0800

    Retriving lift number from document fix index bug 2

--- src/main/java/com/pwned/line/service/LiftAdvisor.java
+++ src/main/java/com/pwned/line/service/LiftAdvisor.java
@@ -22,12 +22,13 @@ public class LiftAdvisor extends DefaultService {
     @Override
     public void payload() throws Exception {
         String lift = "";
+        String ending = "}}";
         MongoDB mongo = new MongoDB("mongodb://admin:admin@ds243085.mlab.com:43085/lift");  //Connecting DB
         MongoCollection collection =  mongo.getCollection("lift");   // Get DB called lift
         Document myDoc = (Document)collection.find().first();     //Get the document; need to use get() to get more documents
         lift = myDoc.toString();
-        int index = lift.indexOf("Lift") + 6;
-        lift = lift.substring(index, index + 2);
+        int index = lift.indexOf("Lift");
+        lift = lift.substring(index , index + 4) + lift.substring(index + 5, lift.indexOf(ending));
         this.fulfillment = this.fulfillment.replace("@LiftAdvisor", lift);
     }
 
