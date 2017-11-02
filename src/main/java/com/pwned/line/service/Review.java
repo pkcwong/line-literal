3351c213434efde96671da9e8e544fbe7679702d
cjwku1209 <cjwku@connect.ust.hk>
2017-11-02 21:43:10 +0800

    bug found

--- src/main/java/com/pwned/line/service/Review.java
+++ src/main/java/com/pwned/line/service/Review.java
@@ -20,23 +20,9 @@ public class Review extends DefaultService {
 		JSONObject apiParam = new JSONObject(this.getParam("parameters").toString());
 		String department = apiParam.getString("department");
 		String courseCode = apiParam.getString("number");
-		try {
-			// add
-			System.out.println(apiParam.toString());
-			apiParam.getString("ReviewAdd");
-			BasicDBObject SELF = new BasicDBObject("uid", this.getParam("uid").toString());
-			System.out.println(SELF);
-			//BasicDBObject cmd = new BasicDBObject("cmd", "review::add");
-			//BasicDBObject data = new BasicDBObject("data", new BasicDBObject("data",  new BasicDBObject().append("department", department).append("code", courseCode)));
-			//BasicDBObject set1 = new BasicDBObject("$set", new BasicDBObject("buff", new BasicDBObject("cmd", cmd)));
-			//BasicDBObject set2 = new BasicDBObject("$set", new BasicDBObject("buff", new BasicDBObject("data", data)));
-			mongo.getCollection("user").updateOne(SELF, new BasicDBObject("$set", new BasicDBObject("buff", "review-add")));
-			//mongo.getCollection("user").updateOne(SELF, set2);
-			//mongo.getCollection("user").updateOne(SELF, new BasicDBObject("$set", new BasicDBObject("buff", new BasicDBObject().append("cmd", "review::add").append("data", new BasicDBObject().append("department", department).append("code", courseCode)))));
-			this.fulfillment = "You can type your detail review here: ";
-
-		} catch (Exception e) {
-			//no add
+		String add = apiParam.getString("ReviewAdd");
+		//find
+		if(add.equals("")){
 			this.fulfillment = "";
 			ArrayList<Document> courseReview = MongoDB.get(mongo.getCollection("courseReview").find());
 			for (int i = 0; i < courseReview.size(); i++) {
@@ -56,7 +42,12 @@ public class Review extends DefaultService {
 				this.fulfillment = "Sorry, no reviews yet. Help us make one! :D :D";
 			}
 		}
+		else{
+			BasicDBObject SELF = new BasicDBObject("uid", this.getParam("uid").toString());
+			mongo.getCollection("user").updateOne(SELF, new BasicDBObject("$set", new BasicDBObject("buff", new BasicDBObject().append("cmd", "review::add").append("data", new BasicDBObject().append("department", department).append("code", courseCode)))));
+			this.fulfillment = "You can type your detail review here: ";
 
+		}
 	}
 
 	@Override
