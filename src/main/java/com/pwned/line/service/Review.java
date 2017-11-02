1ae8221b619b796e84c9f9f2f8511c26db49a899
cjwku1209 <cjwku@connect.ust.hk>
2017-11-02 21:19:34 +0800

    Debugging moved try block

--- src/main/java/com/pwned/line/service/Review.java
+++ src/main/java/com/pwned/line/service/Review.java
@@ -21,8 +21,17 @@ public class Review extends DefaultService {
 		String department = apiParam.getString("department");
 		String courseCode = apiParam.getString("number");
 		try {
-			apiParam.getString("ReviewAdd");
 			// add
+			apiParam.getString("ReviewAdd");
+			BasicDBObject SELF = new BasicDBObject("uid", this.getParam("uid").toString());
+			//BasicDBObject cmd = new BasicDBObject("cmd", "review::add");
+			//BasicDBObject data = new BasicDBObject("data", new BasicDBObject("data",  new BasicDBObject().append("department", department).append("code", courseCode)));
+			//BasicDBObject set1 = new BasicDBObject("$set", new BasicDBObject("buff", new BasicDBObject("cmd", cmd)));
+			//BasicDBObject set2 = new BasicDBObject("$set", new BasicDBObject("buff", new BasicDBObject("data", data)));
+			mongo.getCollection("user").updateOne(SELF, new BasicDBObject("$set", new BasicDBObject("buff", "review::add")));
+			//mongo.getCollection("user").updateOne(SELF, set2);
+			//mongo.getCollection("user").updateOne(SELF, new BasicDBObject("$set", new BasicDBObject("buff", new BasicDBObject().append("cmd", "review::add").append("data", new BasicDBObject().append("department", department).append("code", courseCode)))));
+			this.fulfillment = "You can type your detail review here: ";
 
 		} catch (Exception e) {
 			//no add
@@ -45,15 +54,7 @@ public class Review extends DefaultService {
 				this.fulfillment = "Sorry, no reviews yet. Help us make one! :D :D";
 			}
 		}
-		BasicDBObject SELF = new BasicDBObject("uid", this.getParam("uid").toString());
-		BasicDBObject cmd = new BasicDBObject("cmd", "review::add");
-		//BasicDBObject data = new BasicDBObject("data", new BasicDBObject("data",  new BasicDBObject().append("department", department).append("code", courseCode)));
-		BasicDBObject set1 = new BasicDBObject("$set", new BasicDBObject("buff", new BasicDBObject("cmd", cmd)));
-		//BasicDBObject set2 = new BasicDBObject("$set", new BasicDBObject("buff", new BasicDBObject("data", data)));
-		mongo.getCollection("user").updateOne(SELF, set1);
-		//mongo.getCollection("user").updateOne(SELF, set2);
-		//mongo.getCollection("user").updateOne(SELF, new BasicDBObject("$set", new BasicDBObject("buff", new BasicDBObject().append("cmd", "review::add").append("data", new BasicDBObject().append("department", department).append("code", courseCode)))));
-		this.fulfillment = "You can type your detail review here: ";
+
 	}
 
 	@Override
