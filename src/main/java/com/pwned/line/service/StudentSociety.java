8ca7468bcd9b92ccaa3850642ba48a87324240c5
kwh199703 <kwh199703@hotmail.com>
2017-11-02 19:08:23 +0800

    amended studentSociety class

--- src/main/java/com/pwned/line/service/StudentSociety.java
+++ src/main/java/com/pwned/line/service/StudentSociety.java
@@ -31,7 +31,7 @@ public class StudentSociety extends DefaultService {
         String SocietyCode = apiParam.getString("society");
         HTTP link = new HTTP(SocInfo_URL);
         String societypage = link.get();
-
+        System.out.println(SocietyCode);
         String SocietyName = "";
 
         String[] keywords = {"<a href=\"http://ihome.ust.hk/~","\" target=\"_blank\">", ", HKUSTSU", "</a></td>"};
