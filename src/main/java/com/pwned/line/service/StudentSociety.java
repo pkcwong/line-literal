72e13d4ccd53699a3d03ab8bd124bc4332e42d97
kwh199703 <kwh199703@hotmail.com>
2017-11-03 10:57:01 +0800

    removed debug message

--- src/main/java/com/pwned/line/service/StudentSociety.java
+++ src/main/java/com/pwned/line/service/StudentSociety.java
@@ -28,25 +28,21 @@ public class StudentSociety extends DefaultService {
     @Override
     public void payload() throws Exception {
         String SocietyCode = new JSONObject(this.getParam("parameters").toString()).getString("society");
-        System.out.println(SocietyCode);
+
         HTTP link = new HTTP(SocInfo_URL);
         String societypage = link.get();
-
         String SocietyName = "";
-
         String[] keywords = {"<a href=\"http://ihome.ust.hk/~","\" target=\"_blank\">", ", HKUSTSU", "</a>"};
         String societyURL = SocietyCode+keywords[1];
-
-        String societyweb = societypage.substring(societypage.indexOf(societyURL),societypage.lastIndexOf(SocietyCode));
-        if(societypage.contains(societyURL)){
-            System.out.println("contains");
-            String societynamecode = societyweb.substring(societyweb.indexOf(societyURL)+societyURL.length(), societyweb.indexOf(keywords[3]));
-            SocietyName = "I found some societies for you:\n"+societynamecode;
-            SocietyName = SocietyName+"\ntheir website is\nhttp://ihome.ust.hk/~"+SocietyCode;
-            System.out.println("SocietyName = "+ SocietyName);
-
-        }else{
+        if(societyURL.equals(keywords[1])){
             SocietyName = "Sorry, i can't find a suitable one for you.";
+        }else{
+            String societyweb = societypage.substring(societypage.indexOf(societyURL),societypage.lastIndexOf(SocietyCode));
+            if(societypage.contains(societyURL)){
+                String societynamecode = societyweb.substring(societyweb.indexOf(societyURL)+societyURL.length(), societyweb.indexOf(keywords[3]));
+                SocietyName = "I found some societies for you:\n"+societynamecode;
+                SocietyName = SocietyName+"\ntheir website is\nhttp://ihome.ust.hk/~"+SocietyCode;
+            }
         }
         this.fulfillment=this.fulfillment.replace("@Society::Name", SocietyName);
 
