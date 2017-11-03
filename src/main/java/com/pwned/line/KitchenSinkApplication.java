0d122b8b1ea0ebabfad7b14761d118a7eec21cba
httpak <httpak@connect.ust.hk>
2017-11-03 19:13:26 +0800

    PushWeather Build

--- src/main/java/com/pwned/line/KitchenSinkApplication.java
+++ src/main/java/com/pwned/line/KitchenSinkApplication.java
@@ -1,8 +1,7 @@
 package com.pwned.line;
 
-import com.pwned.line.job.DefaultJob;
 import com.pwned.line.job.PushWeather;
-import org.quartz.*;
+import org.quartz.Scheduler;
 import org.quartz.impl.StdSchedulerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -20,7 +19,7 @@ public class KitchenSinkApplication {
 		SpringApplication.run(KitchenSinkApplication.class, args);
 		Scheduler scheduler = StdSchedulerFactory.getDefaultScheduler();
 		scheduler.start();
-		scheduler.scheduleJob(DefaultJob.buildJob(PushWeather.class), DefaultJob.buildTrigger(5));
+		scheduler.scheduleJob(PushWeather.buildJob(PushWeather.class), PushWeather.buildTrigger(5));
 	}
 
 }
