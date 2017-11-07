46c25d5470bae7995bd485c788d866a248a296c6
Bear <Bear@BeardeMacBook-Pro.local>
2017-11-07 21:25:41 +0800

    Create DialogFlowEventMaker Class

---
+++ src/main/java/com/pwned/line/service/DialogFlowEventMaker.java
@@ -0,0 +1,15 @@
+package com.pwned.line.service;
+
+/***
+ * Service for finding closest lift by location.
+ * Required params: [params]
+ * Reserved tokens: []
+ * Resolved params: []
+ * @author Bear
+ */
+
+public class DialogFlowEventMaker extends DefaultService{
+	public DialogFlowEventMaker(Service serive){
+		super(serive);
+	}
+}
