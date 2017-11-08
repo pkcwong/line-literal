b4686bc8916df608a259453bd52e53145937f18e
httpak <httpak@connect.ust.hk>
2017-11-08 10:00:46 +0800

    default HK

--- src/main/java/com/pwned/line/service/Temperature.java
+++ src/main/java/com/pwned/line/service/Temperature.java
@@ -24,75 +24,16 @@ public class Temperature extends DefaultService{
 		if(city.equals("")){
 		    city = "Hong Kong";
 		}
-		if(city.equals("Hong Kong")){
-			System.out.println("TRUE");
-			System.out.println("TRUE");
-			System.out.println("TRUE");
-			System.out.println("TRUE");
-			System.out.println("TRUE");
-			System.out.println("TRUE");
-			System.out.println("TRUE");
-			System.out.println("TRUE");
-			System.out.println("TRUE");
-			System.out.println("TRUE");
-			System.out.println("TRUE");
-			System.out.println("TRUE");
-			System.out.println("TRUE");
-			System.out.println("TRUE");
-			System.out.println("TRUE");
-			System.out.println("TRUE");
-			System.out.println("TRUE");
-			System.out.println("TRUE");
-			System.out.println("TRUE");
-			System.out.println("TRUE");
-			System.out.println("TRUE");
-			System.out.println("TRUE");
-			System.out.println("TRUE");
-			System.out.println("TRUE");
-			System.out.println("TRUE");
-		}
-		System.out.println("||| " + city + " |||");
-		System.out.println("||| " + city + " |||");
-		System.out.println("||| " + city + " |||");
-		System.out.println("||| " + city + " |||");
-		System.out.println("||| " + city + " |||");
-		System.out.println("||| " + city + " |||");
-		System.out.println("||| " + city + " |||");
-		System.out.println("||| " + city + " |||");
-		System.out.println("||| " + city + " |||");
-		System.out.println("||| " + city + " |||");
-		System.out.println("||| " + city + " |||");
-		System.out.println("||| " + city + " |||");
-		System.out.println("||| " + city + " |||");
-		System.out.println("||| " + city + " |||");
-		System.out.println("||| " + city + " |||");
-		System.out.println("||| " + city + " |||");
-		System.out.println("||| " + city + " |||");
-		System.out.println("||| " + city + " |||");
-		System.out.println("||| " + city + " |||");
-		System.out.println("||| " + city + " |||");
-		System.out.println("||| " + city + " |||");
-		System.out.println("||| " + city + " |||");
-		System.out.println("||| " + city + " |||");
-		System.out.println("||| " + city + " |||");
 		String link = "http://rss.weather.gov.hk/rss/CurrentWeather.xml";
 		HTTP http = new HTTP(link);
 		String temperatureString = http.get();
 		String temperature = "";
 		if(city.equals("Hong Kong")){
-            temperature = temperatureString.substring(temperatureString.indexOf("Air temperature :") + 16, temperatureString.indexOf("Air temperature :") + 18);
+            temperature = temperatureString.substring(temperatureString.indexOf("Air temperature :") + 18, temperatureString.indexOf("Air temperature :") + 20);
             temperature = temperature + "°C";
 		}else if(temperatureString.contains(city)){
 			temperature = temperatureString.substring(temperatureString.indexOf(city) + city.length() + 58, temperatureString.indexOf(city) + city.length() + 60);
 			temperature = temperature + "°C";
-		}else{
-			city = "Hong Kong";
-			if(temperatureString.indexOf("degrees Celsius<br/>") - 1 > temperatureString.indexOf("Air temperature : ") + 18){
-				temperature = temperatureString.substring(temperatureString.indexOf("Air temperature : ") + 18, temperatureString.indexOf("degrees Celsius<br/>") - 1);
-				temperature = temperature + "°C";
-			}else{
-				temperature = (temperatureString.indexOf("degrees Celsius<br/>") - 1) + " " + (temperatureString.indexOf("Air temperature : ") + 18);
-			}
 		}
 		if(temperature == ""){
 			temperature = "not available";
