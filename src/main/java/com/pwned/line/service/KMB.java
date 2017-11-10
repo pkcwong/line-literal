d716d4912387ac1ab27e9f726b74946a5b4b998a
httpak <httpak@connect.ust.hk>
2017-11-10 21:11:13 +0800

    stops to array

--- src/main/java/com/pwned/line/service/KMB.java
+++ src/main/java/com/pwned/line/service/KMB.java
@@ -32,7 +32,14 @@ public class KMB extends DefaultService{
             String info = http.get();
             JSONObject stop = new JSONObject(info);
             JSONArray stops = stop.getJSONArray("stops");
-            stops.getJSONArray(1);
+
+            /*for (int i = 0; i < jArray.length(); i++) {
+                JSONObject object = jArray.optJSONObject(i);
+                Iterator<String> iterator = object.keys();
+                while(iterator.hasNext()) {
+                    String currentKey = iterator.next();
+                }
+            }*/
             JSONArray services = stops.toJSONObject(stops).getJSONArray("services");
             System.out.println(services);
             System.out.println(services);
