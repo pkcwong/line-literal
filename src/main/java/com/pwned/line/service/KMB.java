194024efe7424ec566268c1676939b35d98a18a0
httpak <httpak@connect.ust.hk>
2017-11-10 19:54:04 +0800

    fetch Json

--- src/main/java/com/pwned/line/service/KMB.java
+++ src/main/java/com/pwned/line/service/KMB.java
@@ -22,11 +22,17 @@ public class KMB extends DefaultService{
         String bus = new JSONObject(this.getParam("parameters").toString()).getString("bus");
         String busstop = new JSONObject(this.getParam("parameters").toString()).getString("busstop");
         String eta = "Sorry, there is no bus arriving";
+        if(bus.equals("")||busstop.equals("")){
+            eta = "Please enter a valid bus route or bus stop";
+        }
         if(busstop.equals("HKUST South Gate")){
             String link = "https://citymapper.com/api/1/departures?headways=1&ids=HKStop_HkustSouth_NW_1&region_id=hk-hongkong";
             HTTP http = new HTTP(link);
             String info = http.get();
-            this.fulfillment = this.fulfillment.replace("@kmb::eta", "You requested for the arrival time of the next " + bus + " to " + busstop);
+            JSONObject stop = new JSONObject(info);
+            JSONObject services = stop.getJSONObject("services");
+            String etasecond = services.getJSONArray("headway_seconds_range").toString();
+            this.fulfillment = this.fulfillment.replace("@kmb::eta", "You requested for the arrival time of the next " + bus + " to " + busstop + ", the eta is " + etasecond + " seconds.");
         }
         this.fulfillment = this.fulfillment.replace("@kmb::eta", eta);
     }
