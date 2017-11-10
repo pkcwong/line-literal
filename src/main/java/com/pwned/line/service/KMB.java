0b6b5cf79fdc073d680ec66ab0ffeda3b6dbe1d5
httpak <httpak@connect.ust.hk>
2017-11-10 22:27:42 +0800

    get the eta

--- src/main/java/com/pwned/line/service/KMB.java
+++ src/main/java/com/pwned/line/service/KMB.java
@@ -20,9 +20,11 @@ public class KMB extends DefaultService{
     }
     @Override
     public void payload() throws Exception {
+        String[] route = {"KMB 91M", "KMB 91"};
         String bus = new JSONObject(this.getParam("parameters").toString()).getString("bus");
         String busstop = new JSONObject(this.getParam("parameters").toString()).getString("busstop");
         String eta = "Sorry, there is no bus arriving";
+        int etaminutes = 20;
         if(bus.equals("")||busstop.equals("")){
             eta = "Please enter a valid bus route or bus stop";
         }
@@ -34,47 +36,31 @@ public class KMB extends DefaultService{
             JSONArray stops = stop.getJSONArray("stops");
             JSONObject layer = stops.getJSONObject(0);
             JSONArray services = layer.getJSONArray("services");
-            System.out.println(services);
-            System.out.println(services);
-            System.out.println(services);
-            System.out.println(services);
-            System.out.println(services);
-            System.out.println(services);
-            System.out.println(services);
-            System.out.println(services);
-            System.out.println(services);
+            String[] route_id = new String[services.length()];
+            String[] etasecond = new String[services.length()];
             for(int i = 0; i < services.length(); i++){
-                String route_id = services.getJSONObject(i).getString("route_id");
-                System.out.println(route_id);
-                System.out.println(route_id);
-                System.out.println(route_id);
-                System.out.println(route_id);
-                System.out.println(route_id);
-                System.out.println(route_id);
-                System.out.println(route_id);
-                System.out.println(route_id);
+                route_id[i] = services.getJSONObject(i).getString("route_id");
                 if(services.getJSONObject(i).has("live_departures_seconds")){
-                    String live_departures_seconds = services.getJSONObject(i).getString("live_departures_seconds");
-                    System.out.println(live_departures_seconds);
-                    System.out.println(live_departures_seconds);
-                    System.out.println(live_departures_seconds);
-                    System.out.println(live_departures_seconds);
-                    System.out.println(live_departures_seconds);
-                    System.out.println(live_departures_seconds);
-                    System.out.println(live_departures_seconds);
-                    System.out.println(live_departures_seconds);
-                    System.out.println(live_departures_seconds);
-                    System.out.println(live_departures_seconds);
-                    System.out.println(live_departures_seconds);
-                    System.out.println(live_departures_seconds);
+                    etasecond[i] = services.getJSONObject(i).getString("live_departures_seconds");
+                    etasecond[i] = etasecond[i].substring(1, etasecond[i].length() - 1);
                 }else if(services.getJSONObject(i).has("headway_seconds_range")){
-                    String headway_seconds_range = services.getJSONObject(i).getString("headway_seconds_range");
-                    System.out.println(headway_seconds_range);
-                    System.out.println(headway_seconds_range);
-                    System.out.println(headway_seconds_range);
+                    etasecond[i] = "1200";
                 }
             }
-            this.fulfillment = this.fulfillment.replace("@kmb::eta", "You requested for the arrival time of the next " + bus + " to " + busstop + ", the eta is  seconds.");
+            if(bus.equals(route[0])){
+                for(int i = 0; i < services.length(); i++){
+                    if(route[i].equals("KMBBus91M")){
+                        etaminutes = Integer.parseInt(etasecond[i]) / 60;
+                    }
+                }
+            }else if(bus.equals(route[1])){
+                for(int i = 0; i < services.length(); i++){
+                    if(route[i].equals("KMBBus91")){
+                        etaminutes = Integer.parseInt(etasecond[i]) / 60;
+                    }
+                }
+            }
+            this.fulfillment = this.fulfillment.replace("@kmb::eta", "You requested for the arrival time of the next " + bus + " to " + busstop + ", the estimated time of arrival is " + etaminutes + " minutes.");
         }
         this.fulfillment = this.fulfillment.replace("@kmb::eta", eta);
     }
