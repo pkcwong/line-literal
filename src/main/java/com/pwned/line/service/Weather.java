67392d477fb7057d65fe641780fb11d8d9e9abfc
httpak <httpak@connect.ust.hk>
2017-11-11 22:40:09 +0800

    v154

--- src/main/java/com/pwned/line/service/Weather.java
+++ src/main/java/com/pwned/line/service/Weather.java
@@ -1,15 +1,15 @@
 package com.pwned.line.service;
 
+import com.linecorp.bot.model.action.Action;
+import com.linecorp.bot.model.action.MessageAction;
+import com.linecorp.bot.model.message.TemplateMessage;
+import com.linecorp.bot.model.message.template.CarouselColumn;
+import com.linecorp.bot.model.message.template.CarouselTemplate;
+import com.pwned.line.KitchenSinkController;
 import com.pwned.line.http.HTTP;
-import org.json.JSONObject;
-import java.io.*;
 
-import org.w3c.dom.Document;
-import org.w3c.dom.NodeList;
-import org.xml.sax.InputSource;
-
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
+import java.util.ArrayList;
+import java.util.List;
 
 /***
  * Service for course information.
@@ -27,6 +27,16 @@ public class Weather extends DefaultService{
 	}
 	@Override
 	public void payload() throws Exception {
+		String url = "http://www.weather.gov.hk/images/wxicon/pic62.png";
+		ArrayList<CarouselColumn> nineColumns = new ArrayList<>();
+		List<Action> actions = new ArrayList<>();
+		actions.add(new MessageAction("hi", "hi"));
+		nineColumns.add(new CarouselColumn(url, "title", "Testing", actions));
+		for(int i = 0; i < 20; i++)
+		System.out.println("Before push");
+		KitchenSinkController.push(this.getParam("uid").toString(), new TemplateMessage("Carousel Template", new CarouselTemplate(nineColumns)));
+		for(int i = 0; i < 20; i++)
+		System.out.println("After push");
 		String link = "http://www.hko.gov.hk/wxinfo/currwx/flw.htm";
 		HTTP http = new HTTP(link);
 		String weather = http.get();
