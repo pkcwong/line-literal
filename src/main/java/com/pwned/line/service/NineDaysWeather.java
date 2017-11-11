0367d98b6496763bb14a091c3fd7618fd995237a
httpak <httpak@connect.ust.hk>
2017-11-11 15:21:12 +0800

    v151

--- src/main/java/com/pwned/line/service/NineDaysWeather.java
+++ src/main/java/com/pwned/line/service/NineDaysWeather.java
@@ -41,36 +41,31 @@ public class NineDaysWeather extends DefaultService{
             Date nextday = c.getTime();
             date[dd] = dateFormat.format(nextday);
         }
-        for(int i = 0; i < 20; i++)
-        System.out.println("|||ImageURL|||");
         String imageurlstring = ninedaysweather;
         String[] imageurl = new String[9];
         for (int url = 0; url < 9; url++){
             imageurlstring = imageurlstring.substring(imageurlstring.indexOf(text[1]));
             imageurl[url] = text[0] + imageurlstring.substring(imageurlstring.indexOf("/"), imageurlstring.indexOf("/") + 24);
             imageurlstring = imageurlstring.substring(2);
-            System.out.println(imageurl[url]);
         }
-        for(int i = 0; i < 20; i++)
-            System.out.println("|||Desription|||");
         String desriptionsstring = ninedaysweather;
         String[] desription = new String[9];
         for(int weather = 0; weather < 9; weather++){
             desriptionsstring = desriptionsstring.substring(desriptionsstring.indexOf(text[2]));
             desription[weather] = desriptionsstring.substring(text[2].length() + 4, desriptionsstring.indexOf(text[3]));
-            System.out.println(desription[weather]);
             desriptionsstring = desriptionsstring.substring(2);
         }
         for(int i = 0; i < 20; i++)
             System.out.println("|||Carousel|||");
-        CarouselTemplate ninedays;
+        CarouselTemplate carouselTemplate;
         List<CarouselColumn> nineColumns = new ArrayList<>();
-        for(int days = 0; days < 9; days++){
+        for(int days = 0; days < 5; days++){
             nineColumns.add(days, new CarouselColumn(imageurl[days], date[days], desription[days], null));
         }
-        ninedays = new CarouselTemplate(nineColumns);
+        //confirmTemplate = new ConfirmTemplate("Anonymous Chat System", new PostbackAction("terminate", "anonymous::terminate"), new PostbackAction("cancel", "anonymous::cancel"));
+        carouselTemplate = new CarouselTemplate(nineColumns);
         this.fulfillment = this.fulfillment.replace("@weather::ninedaysweather", "The following is the weather forecast for next 9 days:");
-        KitchenSinkController.push(this.getParam("uid").toString(), new TemplateMessage("Nine Days Weather Forecast", ninedays));
+        KitchenSinkController.push(this.getParam("uid").toString(), new TemplateMessage("Nine Days Weather Forecast", carouselTemplate));
     }
 
 
