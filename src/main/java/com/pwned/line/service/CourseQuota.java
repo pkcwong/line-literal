5afe93ff207de672fbe5b6275265d726a306c60c
cjwku1209 <cjwku@connect.ust.hk>
2017-11-13 20:19:00 +0800

    debugging

--- src/main/java/com/pwned/line/service/CourseQuota.java
+++ src/main/java/com/pwned/line/service/CourseQuota.java
@@ -12,8 +12,8 @@ public class CourseQuota extends DefaultService{
 	public void payload() throws Exception {
 		Course course = new Course("COMP", "2012");
 		course.query();
-		System.out.println("Introduction: " + course.introduction + "\nDepartment: " + course.department + "\nCode: " + course.code  + "\nTitle: " + course.title +
-		"Credit: " + course.credit);
+		System.out.println("Department: " + course.department + "\nCode: " + course.code  + "\nTitle: " + course.title +
+		"\nCredit: " + course.credit);
 		for(int i = 0; i < course.sections.size(); i++){
 			System.out.println("Section name: " + course.sections.get(i).name + "\nSection Code: " + course.sections.get(i).code + "\nQuota: " + course.sections.get(i).quota +
 			"\nEnrol: " + course.sections.get(i).enrol +"\nAvail: " + course.sections.get(i).avail + "\nWait: " + course.sections.get(i).wait);
