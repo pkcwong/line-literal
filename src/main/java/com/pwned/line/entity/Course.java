326ae2c6c03ec02a96bf44ca2f81f16d830fdd90
cjwku1209 <cjwku@connect.ust.hk>
2017-11-13 19:47:14 +0800

    Debugging

--- src/main/java/com/pwned/line/entity/Course.java
+++ src/main/java/com/pwned/line/entity/Course.java
@@ -13,7 +13,7 @@ import java.util.regex.Pattern;
 public class Course {
 
 	private static final String BASE_URL = "https://w5.ab.ust.hk/wcq/cgi-bin/1710/subject/";
-	public static final String REGEX_COURSE = "<div class=\\\"course\\\">[\\s\\S]+?<tr><th>DESCRIPTION<\\/th><td>(.+?)<\\/td><\\/tr>[\\s\\S]+?<h2>(....)\\s(....)\\s-\\s(.+?)\\s\\((\\d)\\sunits\\)<\\/h2>\\s<table class=\\\"sections\\\" width=\\\"1012\\\">\\s([\\s\\S]+?)<\\/td><\\/tr><\\/table>\\s<\\/div>";
+	public static final String REGEX_COURSE = "<div class=\\\"course\\\">[\\s\\S]+?<tr><th>DESCRIPTION<\\/th><td>(.+?)<\\/td><\\/tr>[\\s\\S]+?<h2>(@department)\\s(@code)\\s-\\s(.+?)\\s\\((\\d)\\sunits\\)<\\/h2>\\s<table class=\\\"sections\\\" width=\\\"1012\\\">\\s([\\s\\S]+?)<\\/td><\\/tr><\\/table>\\s<\\/div>";
 	public static final String REGEX_GET_SECTION= "<td align=\"center\".+?\\s?<td>(?:.*) class=(?:.*)>";
 	public static final String REGEX_SECTION_NAME_CODE = ".+?>(\\w{1,3}) \\((\\w{1,4})";
 	public static final String REGEX_GET_ALL_DATE_TIME_ROOM_PROF = "(Mo|Tu|We|Th|Fr|MoTu|MoWe|MoTh|MoFr|TuWe|TuTh|TuFr|WeTh|WeFr|ThFr|MoTuWe|MoTuTh|MoTuFr|MoWeTh|MoWeFr|MoThFr|TuWeTh|TuWeFr|WeThFr) ([^ ]*) - ([^<]*)<\\/td><td>([^<]*).+?instructor\\/([^\"]*)";
@@ -36,10 +36,24 @@ public class Course {
 		HTTP http = new HTTP(BASE_URL + this.department);
 		String response = http.get();
 		//System.out.println(response);
-		Pattern regex_course = Pattern.compile(REGEX_COURSE);
-		//Pattern regex_course = Pattern.compile(this.builder());
-		Matcher matcher_course = regex_course.matcher(response);
-		System.out.println(REGEX_COURSE);
+		//Pattern regex_course = Pattern.compile(REGEX_COURSE);
+		Pattern regex_course = Pattern.compile(this.builder());
+		Matcher matcher_course = regex_course.matcher("<h2>COMP 2012 - Object-Oriented Programming and Data Structures (4 units)</h2>\n" +
+				"<table class=\"sections\" width=\"1012\">\n" +
+				"<tr><th width=\"85\">Section</th><th width=\"190\" style=\"text-align: left\">Date & Time</th><th width=\"160\" style=\"text-align: left\">Room</th><th width=\"190\" style=\"text-align: left\">Instructor</th><th width=\"45\">Quota</th><th width=\"45\">Enrol</th><th width=\"45\">Avail</th><th width=\"45\">Wait</th><th width=\"81\">Remarks</th></tr>\n" +
+				"<tr class=\"newsect secteven\">\n" +
+				"<td align=\"center\">L1 (1807)</td>\n" +
+				"<td>WeFr 04:30PM - 05:50PM</td><td>Rm 4619, Lift 31-32 (126)</td><td><a href=\"/wcq/cgi-bin/1710/instructor/TSOI, Yau Chat\">TSOI, Yau Chat</a></td><td align=\"center\">120</td><td align=\"center\">116</td><td align=\"center\">4</td><td align=\"center\">0</td><td align=\"center\">&nbsp;</td></tr><tr class=\"newsect sectodd\">\n" +
+				"<td align=\"center\">L2 (1809)</td>\n" +
+				"<td>TuTh 03:00PM - 04:20PM</td><td>Rm 2465, Lift 25-26 (122)</td><td><a href=\"/wcq/cgi-bin/1710/instructor/QUAN, Long\">QUAN, Long</a></td><td align=\"center\">87</td><td align=\"center\">74</td><td align=\"center\">13</td><td align=\"center\">0</td><td align=\"center\">&nbsp;</td></tr><tr class=\"newsect secteven\">\n" +
+				"<td align=\"center\">LA1 (1811)</td>\n" +
+				"<td>Th 01:00PM - 02:50PM</td><td>Rm 4210, Lift 19 (67)</td><td><a href=\"/wcq/cgi-bin/1710/instructor/QUAN, Long\">QUAN, Long</a><br><a href=\"/wcq/cgi-bin/1710/instructor/TSOI, Yau Chat\">TSOI, Yau Chat</a></td><td align=\"center\">67</td><td align=\"center\">66</td><td align=\"center\">1</td><td align=\"center\">0</td><td align=\"center\">&nbsp;</td></tr><tr class=\"newsect sectodd\">\n" +
+				"<td align=\"center\">LA2 (1813)</td>\n" +
+				"<td>Th 10:30AM - 12:20PM</td><td>Rm 4210, Lift 19 (67)</td><td><a href=\"/wcq/cgi-bin/1710/instructor/QUAN, Long\">QUAN, Long</a><br><a href=\"/wcq/cgi-bin/1710/instructor/TSOI, Yau Chat\">TSOI, Yau Chat</a></td><td align=\"center\">65</td><td align=\"center\">61</td><td align=\"center\">4</td><td align=\"center\">0</td><td align=\"center\">&nbsp;</td></tr><tr class=\"newsect secteven\">\n" +
+				"<td align=\"center\">LA3 (3878)</td>\n" +
+				"<td>Tu 11:30AM - 01:20PM</td><td>Rm 4210, Lift 19 (67)</td><td><a href=\"/wcq/cgi-bin/1710/instructor/QUAN, Long\">QUAN, Long</a><br><a href=\"/wcq/cgi-bin/1710/instructor/TSOI, Yau Chat\">TSOI, Yau Chat</a></td><td align=\"center\">67</td><td align=\"center\">63</td><td align=\"center\">4</td><td align=\"center\">0</td><td align=\"center\">&nbsp;</td></tr></table>\n" +
+				"</div>");
+		System.out.println(this.builder());
 		while (matcher_course.find()){
 			System.out.println("0.0");
 			this.introduction = matcher_course.group(1);
