04dc41cd7f18e0b3d8419141be9ca7cd99fa87f2
cjwku1209 <cjwku@connect.ust.hk>
2017-11-13 18:54:45 +0800

    Debugging

--- src/main/java/com/pwned/line/entity/Course.java
+++ src/main/java/com/pwned/line/entity/Course.java
@@ -40,17 +40,12 @@ public class Course {
 		Pattern regex_course = Pattern.compile(this.builder());
 		Matcher matcher_course = regex_course.matcher(response);
 		System.out.println(this.builder());
-		try{
-			if (matcher_course.find()){
-				System.out.println("0.0");
-				this.introduction = matcher_course.group(1);
-				this.title = matcher_course.group(4);
-				this.credit = matcher_course.group(5);
-				section_block = matcher_course.group(6);
-			}
-		}
-		catch (Exception e){
-			e.printStackTrace();
+		if (matcher_course.matches()){
+			System.out.println("0.0");
+			this.introduction = matcher_course.group(1);
+			this.title = matcher_course.group(4);
+			this.credit = matcher_course.group(5);
+			section_block = matcher_course.group(6);
 		}
 		System.out.println("1");
 		Pattern regex_section_info = Pattern.compile(REGEX_GET_SECTION);
