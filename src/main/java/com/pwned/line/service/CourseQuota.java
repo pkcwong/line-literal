85f849ab8eb8b83cc77e5d048d61fe1d930d9613
cjwku1209 <cjwku@connect.ust.hk>
2017-11-17 17:47:06 +0800

    added DialogFlow class

--- src/main/java/com/pwned/line/service/CourseQuota.java
+++ src/main/java/com/pwned/line/service/CourseQuota.java
@@ -10,13 +10,8 @@ public class CourseQuota extends DefaultService{
 
 	public CourseQuota(Service service){ super(service); }
 
-	private static final String API_AI_ACCESS_TOKEN = "bd33003adb6c4a5d8da4e278eaee8ee6";
-
 	@Override
 	public void payload() throws Exception {
-		JSONObject json = new ApiAI(API_AI_ACCESS_TOKEN, this.getParam("uid").toString(), this.fulfillment).execute();
-		this.fulfillment = json.getJSONObject("result").getJSONObject("fulfillment").getString("speech");
-		this.setParam("parameters", json.getJSONObject("result").getJSONObject("parameters"));
 		JSONObject apiParam = new JSONObject(this.getParam("parameters").toString());
 		String department = apiParam.getString("department");
 		String code = apiParam.getString("number");
