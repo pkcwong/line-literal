762bd94f71682f5f2eee8018421fb0ac01fd648e
cjwku1209 <cjwku@connect.ust.hk>
2017-11-17 18:18:16 +0800

    printing debug message

--- src/main/java/com/pwned/line/entity/Course.java
+++ src/main/java/com/pwned/line/entity/Course.java
@@ -41,6 +41,7 @@ public class Course {
 			this.credit = matcher_course.group(4);
 			section_block = matcher_course.group(5);
 		}
+		System.out.println(section_block);
 		Pattern regex_section_info = Pattern.compile(REGEX_GET_SECTION);
 		Matcher matcher_section = regex_section_info.matcher(section_block);
 		ArrayList<String> section_info = new ArrayList<>();
