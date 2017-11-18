5b0ebb65bd314bb49ec36eca09f8bf7b9c6eaf22
httpak <httpak@connect.ust.hk>
2017-11-18 15:47:07 +0800

    201

--- src/main/java/com/pwned/line/KitchenSinkApplication.java
+++ src/main/java/com/pwned/line/KitchenSinkApplication.java
@@ -14,7 +14,7 @@ import java.nio.file.Path;
 public class KitchenSinkApplication {
 
 	public static Path downloadedContentDir;
-	public static int builtVersion = 200;
+	public static int builtVersion = 201;
 	public static String version = "0.2.2";
 
 	public static void main(String[] args) throws Exception {
--- src/main/java/com/pwned/line/service/DialogFlowKMB.java
+++ src/main/java/com/pwned/line/service/DialogFlowKMB.java
@@ -11,23 +11,23 @@ import org.json.JSONObject;
  * Resolved params: [parameters]
  * @author Timothy Pak
  */
-public class DialogFlowKMB extends DefaultService {
+public class DialogFlowKMB extends DefaultService{
 
     private static String API_AI_ACCESS_TOKEN = "ebfd089f4d4b4dcdb0bc88acdb3b0e81";
 
-    public DialogFlowKMB(Service service) {
+    public DialogFlowKMB(Service service){
         super(service);
     }
 
     @Override
-    public void payload() throws Exception {
+    public void payload() throws Exception{
         JSONObject response = new ApiAI(API_AI_ACCESS_TOKEN, this.getParam("uid").toString(), this.fulfillment).execute();
         this.setParam("parameters", response.getJSONObject("result").getJSONObject("parameters"));
         this.fulfillment = response.getJSONObject("result").getJSONObject("fulfillment").getString("speech");
     }
 
     @Override
-    public Service chain() throws Exception {
+    public Service chain() throws Exception{
         return new KMB(this).resolve().get();
     }
 
