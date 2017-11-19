1030f9fbc37da7057b5f2f37aeb6dd49b345c939
cjwku1209 <cjwku@connect.ust.hk>
2017-11-19 18:25:32 +0800

    fixed course quota

---
+++ src/test/java/com/pwned/line/service/CourseQuotaTest.java
@@ -0,0 +1,20 @@
+package com.pwned.line.service;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CourseQuotaTest {
+
+	@Test
+	public void payload() throws Exception {
+		Service service = new DialogFlowCourseQuota(new DefaultService("Course Quota of COMP 3111"));
+		service.setParam("uid", "junit");
+		service.setParam("replyToken", "junit");
+		service.setParam("timestamp", "junit");
+		service.payload();
+		//assertEquals();
+	}
+
+}
