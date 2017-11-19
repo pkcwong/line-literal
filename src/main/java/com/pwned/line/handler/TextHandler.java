24f0eab835f8dd688a45defc7063a4cfd4746b70
Christopher Wong <christopherpk1996@gmail.com>
2017-11-20 02:44:33 +0800

    fallback thread Exception

--- src/main/java/com/pwned/line/handler/TextHandler.java
+++ src/main/java/com/pwned/line/handler/TextHandler.java
@@ -41,7 +41,7 @@ public class TextHandler {
 				service.dump();
 			} catch (Exception e) {
 				e.printStackTrace();
-				KitchenSinkController.push(event.getSource().getUserId(), new TextMessage(e.getClass().getSimpleName()));
+				KitchenSinkController.push(event.getSource().getUserId(), new TextMessage("Invalid input"));
 			}
 			KitchenSinkController.reply(event.getReplyToken(), new TextMessage(service.getFulfillment()));
 			return null;
