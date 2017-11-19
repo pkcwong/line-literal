e322e2e2c65207fdac0c6da41eca846c3fe065d3
cjwku1209 <cjwku@connect.ust.hk>
2017-11-19 21:03:43 +0800

    Done

--- src/test/java/com/pwned/line/service/CourseQuotaTest.java
+++ src/test/java/com/pwned/line/service/CourseQuotaTest.java
@@ -1,11 +1,24 @@
 package com.pwned.line.service;
 
+import org.json.JSONObject;
 import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class CourseQuotaTest {
+
+	@Test
+	public void payload0() throws Exception {
+		Service service = new DialogFlowCourseQuota(new DefaultService("Course Quota of COMP 3111"));
+		service.setParam("uid", "junit");
+		service.setParam("replyToken", "junit");
+		service.setParam("timestamp", "junit");
+		service.payload();
+		CourseQuota courseQuota = new CourseQuota(service);
+		courseQuota.payload();
+		assertEquals("", courseQuota.getFulfillment());
+	}
 
 	@Test
 	public void payload1() throws Exception {
@@ -20,8 +33,8 @@ public class CourseQuotaTest {
 	}
 
 	@Test
-	public void payload0() throws Exception {
-		Service service = new DialogFlowCourseQuota(new DefaultService("Course Quota of COMP 3111"));
+	public void TBA() throws Exception {
+		Service service = new DialogFlowCourseQuota(new DefaultService("Course Quota of COMP 7990"));
 		service.setParam("uid", "junit");
 		service.setParam("replyToken", "junit");
 		service.setParam("timestamp", "junit");
@@ -30,4 +43,17 @@ public class CourseQuotaTest {
 		courseQuota.payload();
 		assertEquals("", courseQuota.getFulfillment());
 	}
+//	@Test
+//	public void chain0() throws Exception {
+//		Service service = new DialogFlowCourseQuota(new DefaultService(""));
+//		service.setParam("parameters", new JSONObject().append("department", "COMP").append("number", "3111"));
+//		assertEquals(CourseQuota.class, service.chain().getClass());
+//	}
+
+	@Test
+	public void chain1() throws Exception {
+		Service service = new CourseQuota(new DefaultService(""));
+		assertEquals(CourseQuota.class, service.chain().getClass());
+	}
+
 }
