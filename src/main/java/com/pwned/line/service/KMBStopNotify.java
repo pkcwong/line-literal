b9c74c681dc4845d6ba991f1eaee60e0d7dafa53
httpak <httpak@connect.ust.hk>
2017-11-19 15:14:01 +0800

    211

--- src/main/java/com/pwned/line/service/KMBStopNotify.java
+++ src/main/java/com/pwned/line/service/KMBStopNotify.java
@@ -20,11 +20,19 @@ public class KMBStopNotify extends DefaultService{
 
 	@Override
 	public void payload() throws Exception{
-		MongoDB mongo = new MongoDB(System.getenv("MONGODB_URI"));
-		BasicDBObject SELF = new BasicDBObject().append("uid", this.getParam("uid").toString());
-		ArrayList<Document> kmb = MongoDB.get(mongo.getCollection("kmb").find(SELF));
 		this.fulfillment = this.fulfillment.replace("@kmb::stop", "You are not in our list of notification.");
+		MongoDB mongo = new MongoDB(System.getenv("MONGODB_URI"));
+		for(int i = 0; i < 20; i++)
+			System.out.println("1st");
+		BasicDBObject SELF = new BasicDBObject().append("uid", this.getParam("uid").toString());
+		for(int i = 0; i < 20; i++)
+			System.out.println("2nd");
+		ArrayList<Document> kmb = MongoDB.get(mongo.getCollection("kmb").find(SELF));
+		for(int i = 0; i < 20; i++)
+			System.out.println("3rd");
 		if(kmb.size() != 0){
+			for(int i = 0; i < 20; i++)
+				System.out.println("4th");
 			mongo.getCollection("user").deleteOne(SELF);
 			this.fulfillment = this.fulfillment.replace("@kmb::stop", "Ok. We will stop notify you about bus arrival time.");
 		}
