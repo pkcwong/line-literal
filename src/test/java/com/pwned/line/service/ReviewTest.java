d8bc44250c959756bdaab32052d71ae683033007
cjwku1209 <cjwku@connect.ust.hk>
2017-11-19 17:57:17 +0800

    Junit test case for Review is done

--- src/test/java/com/pwned/line/service/ReviewTest.java
+++ src/test/java/com/pwned/line/service/ReviewTest.java
@@ -1,6 +1,7 @@
 package com.pwned.line.service;
 
 import com.pwned.line.web.MongoDB;
+import org.json.JSONObject;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -10,21 +11,12 @@ import static org.junit.Assert.*;
 public class ReviewTest {
 	@Before
 	public void setUp() throws Exception {
-		new MongoDB(System.getenv("MONGODB_URI")).drop("department");
-		new MongoDB(System.getenv("MONGODB_URI")).drop("number");
-		new MongoDB(System.getenv("MONGODB_URI")).drop("ReviewAdd");
-		new MongoDB(System.getenv("MONGODB_URI")).drop("user");
-		new MongoDB(System.getenv("MONGODB_URI")).drop("anonymous");
-		try {
-			Service service = new MasterController(new DefaultService("review"));
-			service.setParam("uid", "junit");
-			service.setParam("replyToken", "junit");
-			service.setParam("timestamp", "junit");
-			service.resolve().get();
-		} catch (Exception e) {
-			e.printStackTrace();
-			assert false;
-		}
+		new MongoDB(System.getenv("MONGODB_URI")).drop("CourseReview");
+		Service service = new MasterController(new DefaultService("review"));
+		service.setParam("uid", "junit");
+		service.setParam("replyToken", "junit");
+		service.setParam("timestamp", "junit");
+		service.resolve().get();
 	}
 
 //	@After
@@ -38,9 +30,48 @@ public class ReviewTest {
 		service.setParam("replyToken", "junit");
 		service.setParam("timestamp", "junit");
 		service.payload();
-		assertEquals ("COMP", service.getParam("department").toString());
-		assertEquals ("3111", service.getParam("number").toString());
-		assertEquals ("", service.getParam("ReviewAdd").toString());
+		assertEquals ("COMP", new JSONObject(service.getParam("parameters").toString()).getString("department"));
+		assertEquals ("3111", new JSONObject(service.getParam("parameters").toString()).getString("number"));
+		assertEquals ("", new JSONObject(service.getParam("parameters").toString()).getString("ReviewAdd"));
+	}
+
+	@Test
+	public void ReviewAddPayload() throws Exception {
+		{
+			Service service = new DialogFlowReview(new DefaultService("Add review for COMP 3111"));
+			service.setParam("uid", "junit");
+			service.setParam("replyToken", "junit");
+			service.setParam("timestamp", "junit");
+			service.payload();
+			service = new Review(service);
+			service.payload();
+			assertEquals("You can type your detail review here: ", service.getFulfillment());
+		}
+		{
+			Service service = new ReviewAdd(new DefaultService("I LOVE DEBAGA <3"));
+			service.setParam("uid", "junit");
+			service.setParam("replyToken", "junit");
+			service.setParam("timestamp", "junit");
+			service.payload();
+			assertEquals("Your course review had been added", service.getFulfillment());
+		}
+		{
+			Service service = new DialogFlowReview(new DefaultService("Review of COMP 3111"));
+			service.setParam("uid", "junit");
+			service.setParam("replyToken", "junit");
+			service.setParam("timestamp", "junit");
+			service.payload();
+			service = new Review(service);
+			service.payload();
+			assertEquals("Here is a random review of COMP3111:\nReview: I LOVE DEBAGA <3", service.getFulfillment());
+		}
+
+	}
+
+	@Test
+	public void ReviewAddChain() throws Exception {
+		Service service = new ReviewAdd(new DefaultService(""));
+		assertEquals(service, service.chain());
 	}
 
 }
