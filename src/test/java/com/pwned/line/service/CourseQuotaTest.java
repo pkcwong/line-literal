3c5d051bf3ddd6e064e8123b30d7ad8eca1bc1fc
cjwku1209 <cjwku@connect.ust.hk>
2017-11-19 19:55:06 +0800

    debug

--- src/test/java/com/pwned/line/service/CourseQuotaTest.java
+++ src/test/java/com/pwned/line/service/CourseQuotaTest.java
@@ -8,14 +8,27 @@ import static org.junit.Assert.*;
 public class CourseQuotaTest {
 
 	@Test
-	public void payload() throws Exception {
+	public void payload0() throws Exception {
 		Service service = new DialogFlowCourseQuota(new DefaultService("Course Quota of COMP 3111"));
 		service.setParam("uid", "junit");
 		service.setParam("replyToken", "junit");
 		service.setParam("timestamp", "junit");
 		service.payload();
+		CourseQuota courseQuota = new CourseQuota(service);
+		courseQuota.payload();
+		assertEquals("", courseQuota.getFulfillment());
+	}
 
-		assertEquals();
+	@Test
+	public void payload1() throws Exception {
+		Service service = new DialogFlowCourseQuota(new DefaultService("Course Quota of COMP 3001"));
+		service.setParam("uid", "junit");
+		service.setParam("replyToken", "junit");
+		service.setParam("timestamp", "junit");
+		service.payload();
+		CourseQuota courseQuota = new CourseQuota(service);
+		courseQuota.payload();
+		assertEquals("Sorry COMP3001 could not be found in Course Quota Page", courseQuota.getFulfillment());
 	}
 
 }
